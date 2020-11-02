74ea8a90c58c4a461fec4b929d226ae24176bafe
Pranshu Malhotra <pranshudee8@gmail.com>
2020-11-02 12:29:19 +0530

    updated-layout-file

--- app/src/main/java/edu/stanford/rkpandey/biggernumberwwcode/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/biggernumberwwcode/MainActivity.kt
@@ -4,22 +4,28 @@ import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import edu.stanford.rkpandey.biggernumberwwcode.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         assignNumbersToButtons()
-        btnLeft.setOnClickListener {
+        binding.btnLeft.setOnClickListener {
             checkAnswer(true)
             assignNumbersToButtons()
         }
 
-        btnRight.setOnClickListener {
+        binding.btnRight.setOnClickListener {
             checkAnswer(false)
             assignNumbersToButtons()
         }
@@ -32,13 +38,15 @@ class MainActivity : AppCompatActivity() {
         while (num2 == num1) {
             num2 = r.nextInt(10)
         }
-        btnLeft.text = "$num1"
-        btnRight.text = "$num2"
+        binding.apply {
+            btnLeft.text = "$num1"
+            binding.btnRight.text = "$num2"
+        }
     }
 
     private fun checkAnswer(isLeftButtonSelected: Boolean) {
-        val n1 = btnLeft.text.toString().toInt()
-        val n2 = btnRight.text.toString().toInt()
+        val n1 = binding.btnLeft.text.toString().toInt()
+        val n2 = binding.btnRight.text.toString().toInt()
         val isAnswerCorrect = if (isLeftButtonSelected) n1 > n2 else n2 > n1
         val toastMessage: String
         val backgroundColor: Int
